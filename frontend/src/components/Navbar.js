4a86f288adb2bf109c588349e610b9879596ea15
Rimpy <singhrimpy6406@gmail.com>
2026-04-21 15:03:20 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
+
+
+
+
 export default function Navbar() {
   const { user, logout } = useAuth();
   const location = useLocation();
